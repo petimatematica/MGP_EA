699b58bc4baa1b8d6cf861c28bd7c5d1e8e5d9b0
EmanuelMQueiroz <106535637+EmanuelMQueiroz@users.noreply.github.com>
2024-04-08 18:59:21 -0300

    Create dixonpricedim2.jl

---
+++ dixonpricedim2.jl
@@ -0,0 +1,22 @@
+using LinearAlgebra
+
+function f(x)
+    return (x[1] - 1)^2 + 2 * (2 * x[2]^2 - x[1])^2
+end
+
+function gradf(x)
+    df_dx = 6*x[1] - 8*x[2]^2 - 2
+    df_dy = 32*x[2]^3 - 16*x[1]*x[2]
+    return [df_dx, df_dy]
+end
+
+function projf(x)
+    return [x[1],x[2]]-(dot([x[1],x[2]], [-1/sqrt(2),1]))/(3/2)*[-1/sqrt(2),1]
+end
+
+x = [1; 1/sqrt(2)]
+
+println("f(x) = ", f(x))
+println("gradf(x) = ", gradf(x))
+println("projf(x) = ", projf(x))
+println("produtoint = ", dot([-1/sqrt(2),1], projf(x)))
