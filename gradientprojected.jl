cdcd5744fb8a9fcdbed54558758aa13cd3aedb4f
EmanuelMQueiroz <106535637+EmanuelMQueiroz@users.noreply.github.com>
2024-04-08 09:52:10 -0300

    Update gradientprojected.jl

--- gradientprojected.jl
+++ gradientprojected.jl
@@ -2,12 +2,12 @@
 # Gradient Project Method
 #
 
+using LinearAlgebra, DataFrames
+include("dixonprice.jl")
+
 function gradientprojected(f, grad_f, projection, x_0, α, tol, maxiter)
+
 
     
 end
 
-conjunto = [1,2,3,4,5]
-mínimo = minimum(conjunto)
-println
-println("O mínimo do conjunto é: ", minimo)
