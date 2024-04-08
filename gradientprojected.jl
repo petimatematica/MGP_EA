008e190327a2fdc8d61a426079ed6512b1588838
EmanuelMQueiroz <106535637+EmanuelMQueiroz@users.noreply.github.com>
2024-04-08 08:12:39 -0300

    Create gradientprojected.jl

---
+++ gradientprojected.jl
@@ -0,0 +1,13 @@
+#
+# Gradient Project Method
+#
+
+function gradientprojected(f, grad_f, projection, x_0, α, tol, maxiter)
+
+    
+end
+
+conjunto = [1,2,3,4,5]
+mínimo = minimum(conjunto)
+println
+println("O mínimo do conjunto é: ", minimo)
