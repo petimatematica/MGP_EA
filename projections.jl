2fe2d652acc61303ab8c328de09b87bfeee121d7
EmanuelMQueiroz <106535637+EmanuelMQueiroz@users.noreply.github.com>
2024-04-11 16:58:36 -0300

    Create projections.jl

---
+++ projections.jl
@@ -0,0 +1,25 @@
+#
+# Funções de projeções para diferentes conjuntos
+#
+
+using LinearAlgebra
+
+## O CONJUNTO C É UMA BOLA DE RAIO δ E CENTRO y ##
+
+function projection(x)
+    norm_yx = norm(y-x)
+    if norm_yx <= δ
+        return x
+    else
+        return y + δ*(y-x) / norm_yx
+    end
+end
+
+# Parâmetros de projeção
+x = [5,10,13]
+δ = 2.0
+y = [0,1,2]
+
+println("Projeção de x em C: ", projection(x, y, δ))
+
+## O CONJUNTO C É UMA BOLA DE RAIO δ E CENTRO y ##
