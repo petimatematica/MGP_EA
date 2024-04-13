34bf8cbddfa65d1612eec5921ded6b1b4a3f2deb
EmanuelMQueiroz <106535637+EmanuelMQueiroz@users.noreply.github.com>
2024-04-13 16:44:06 -0300

    Update dixonpricedim2.jl

--- dixonpricedim2.jl
+++ dixonpricedim2.jl
@@ -1,4 +1,4 @@
-using LinearAlgebra
+# Dixon-Price com dimensão 2
 
 function f(x)
     return (x[1] - 1)^2 + 2 * (2 * x[2]^2 - x[1])^2
@@ -13,8 +13,6 @@ end
 function projf(x)
     return [x[1],x[2]]-(dot([x[1],x[2]], [-1/sqrt(2),1]))/(3/2)*[-1/sqrt(2),1]
 end
-
-x = [1; 1/sqrt(2)]
 
 # println("f(x) = ", f(x))
 # println("∇f(x) = ", ∇f(x))
