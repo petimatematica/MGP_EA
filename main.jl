85e62d540a420f4475936eeed4ed085e402bfbf9
EmanuelMQueiroz <106535637+EmanuelMQueiroz@users.noreply.github.com>
2024-04-15 19:45:48 -0300

    Update main.jl

--- main.jl
+++ main.jl
@@ -1,6 +1,6 @@
 # Chamar todos os arquivos
 
-include("dixonpricedim2.jl")
+include("dixonprice.jl")
 include("projections.jl")
 include("GPA2.jl")
 include("projectedgradient.jl")
@@ -9,10 +9,10 @@ using LinearAlgebra, DataFrames
 
 # Definição dos Parâmetros
 
-n = 2 # Dimensão da Função Dixon-Price
-y = [2,2] # Centro do conjunto viável (Bola)
-δ = 4.0 # Raio da Bola
-x0 = [1,2] # Ponto inicial
+x = [1,2,5] # Ponto inicial
+n = length(x) # Dimensão da Função Dixon-Price
+y = [1,0,0] # Centro do conjunto viável (Bola)
+δ = 3.0 # Raio da Bola
 σ = 1.e-4 # Parâmetro da Busca de Armijo
 ε = 1.e-5 # Critério de parada do Método
 β_inicial = 1.0 # Comprimento de passo inicial
@@ -20,7 +20,7 @@ max_iter = 10000 # Máximo de iteradas do Método
 imax_iter = 100 # Máximo de iteradas da estratégia
 
 # Chamada da função gradientproj
-resultado = gradienteproj(f, ∇f, x0, ε, max_iter, GPA2)
+resultado = gradienteproj(f, ∇f, x, ε, max_iter, GPA2)
 
 # Exibir o resultado
 println(resultado[3])
