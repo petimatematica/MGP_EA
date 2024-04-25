052340518a99a3c411dbca404da105392fa98d81
EmanuelMQueiroz <106535637+EmanuelMQueiroz@users.noreply.github.com>
2024-04-24 23:32:45 -0300

    Update projectedgradient.jl

--- projectedgradient.jl
+++ projectedgradient.jl
@@ -8,7 +8,8 @@ function gradienteproj(f, ∇f, x, ε, max_iter, strategy)
     # Listas para armazenar os resultados
     fvals = Float64[]
     gradnorms = Float64[]
-    stepsizes = Float64[]
+    stepsizes_β = Float64[]
+    stepsizes_γ = Float64[]
     
     # Inicialização
     iter = 0
@@ -34,19 +35,21 @@ function gradienteproj(f, ∇f, x, ε, max_iter, strategy)
 
         if strategy == "GPA1"
         # GPA1
-            newsteps = GPA1(x, f, ∇f, projection, σ, imax_iter, γ_inicial) # GPA1
+            newsteps = GPA1(x, f, ∇f, projection, σ, imax_iter, γ_inicial, β_inicial) # GPA1
             z = projection(x - newsteps[2]  * ∇f(x)) # GPA1
             x = x + newsteps[1] * (z - x) # GPA1
-            push!(stepsizes, newsteps[1])   
+            push!(stepsizes_β, newsteps[2])
+            push!(stepsizes_γ, newsteps[1])   
         else
         # GPA2
             β = GPA2(x, f, ∇f, projection, σ, imax_iter, β_inicial)  # GPA2
             x = projection(x - β * ∇fx) # GPA2
-            push!(stepsizes, β) # Armazena o comprimento de passo na lista stepsizes     
+            push!(stepsizes_γ, 1.0)
+            push!(stepsizes_β, β) # Armazena o comprimento de passo na lista stepsizes     
         end    
     end
 
-    info = DataFrame(fvals = fvals, gradnorms = gradnorms, stepsizes = stepsizes)
+    info = DataFrame(fvals = fvals, gradnorms = gradnorms, stepsizes_β = stepsizes_β, stepsizes_γ = stepsizes_γ)
     
     return (x, f(x), info)
 end
