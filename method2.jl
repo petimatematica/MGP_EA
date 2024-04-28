3cea5aa481c2aa9ec2fd9dbed57c6a8511944bfb
EmanuelMQueiroz <106535637+EmanuelMQueiroz@users.noreply.github.com>
2024-04-28 13:46:25 -0300

    Update method2.jl

--- method2.jl
+++ method2.jl
@@ -7,11 +7,11 @@ function GPA2(x, f, ∇f, projection, σ, min_step, β_inicial)
  
     while β < min_step 
      z_kj = projection(x - β * 2.0^(-j) * ∇f(x)) # Calcula o ponto z_kj
-     stptest = f(z_kj) - f(x) + σ * dot(∇f(x), x - z_kj) # Testa Armijo para o z_kj obtido
+     stptest = f(z_kj) - f(x) + σ * dot(∇f(x), x - z_kj) # Armijo
  
      #println(j, " ", stptest)
  
-       if stptest > 0.0 # Se a condição de Armijo não for satisfeita, testa o próximo j   
+       if stptest > 0.0 # Armijo test   
         j += 1 
         else
         β = β_inicial * 2.0^(-j)
@@ -25,40 +25,50 @@ function GPA2(x, f, ∇f, projection, σ, min_step, β_inicial)
 
  function method2(f, ∇f, x, ε, max_iter, GPA2)
 
-    # Listas para armazenar os resultados
     fvals = Float64[]
     gradnorms = Float64[]
     stepsizes_β = Float64[]
     stepsizes_γ = Float64[]
+    iteration_time = Float64[]
     
-    # Inicialização
+    # Initialization
     iter = 0
     x = x0
+    t0 = time()
     
     while true
-
-        ∇fx = ∇f(x) # Calcula o gradiente no ponto atual
-        fx = f(x) # Calcula o valor da função no ponto atual
-        gradnorm = norm(∇fx) # Calcula a norma do gradiente no ponto atual
+        it0 = time()
+        ∇fx = ∇f(x) 
+        fx = f(x)
+        gradnorm = norm(∇fx)
         
-        # Verifica o critério de parada
+        # First stopping condition
         proj_stop = x - projection(x - ∇fx) 
-        if norm(proj_stop) < ε || iter > max_iter
+        if norm(proj_stop) < ε
+            println("The solution has found!")
             break
         end
 
-        push!(fvals, fx) # Armazena o valor da função na lista fvals
-        push!(gradnorms, gradnorm) # Armazena o valor do gradiente da função na lista gradnorms
+        push!(fvals, fx)
+        push!(gradnorms, gradnorm)
         
-        # Atualiza o contador de iterações
-        iter += 1   
-        β = GPA2(x, f, ∇f, projection, σ, imax_iter, β_inicial)  # GPA2
-        x = projection(x - β * ∇fx) # GPA2
+        # Update iteration
+        iter += 1
+        it = time() - it0   
+        β = GPA2(x, f, ∇f, projection, σ, imax_iter, β_inicial)  
+        x = projection(x - β * ∇fx) 
+        push!(iteration_time, it)
         push!(stepsizes_γ, 1.0)
-        push!(stepsizes_β, β) # Armazena o comprimento de passo na lista stepsizes      
+        push!(stepsizes_β, β) 
+        
+        # Second stopping condition
+        if iter > max_iter
+            println("Maximum of iterations was achieved! Stopping...")
+            break
+        end
     end
 
-    info = DataFrame(fvals = fvals, gradnorms = gradnorms, stepsizes_β = stepsizes_β, stepsizes_γ = stepsizes_γ)
-    
-    return (x, f(x), info)
+    info = DataFrame(fvals = fvals, gradnorms = gradnorms, stepsizes_β = stepsizes_β, stepsizes_γ = stepsizes_γ, iteration_time = iteration_time)
+    et = time() - t0
+    return (x, f(x), info, et)
 end
