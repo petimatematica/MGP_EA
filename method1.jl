02aebb1604e5795c5ce1e5a544f40a1c2f25e246
EmanuelMQueiroz <106535637+EmanuelMQueiroz@users.noreply.github.com>
2024-05-11 10:39:38 -0300

    Update method1.jl

--- method1.jl
+++ method1.jl
@@ -1,4 +1,4 @@
-## Projected Gradient Method with GPA1 ##
+# ## Projected Gradient Method with GPA1 ##
 
 function GPA1(x, f, ∇f, projection, σ, min_step, γ_inicial, β_inicial)
    
@@ -7,11 +7,10 @@ function GPA1(x, f, ∇f, projection, σ, min_step, γ_inicial, β_inicial)
     j = 0
     z_k = projection(x - β * ∇f(x))
 
-    while γ < min_step 
-     stptest = f(x + 2.0^(-j) * (z_k - x)) - f(x) - σ * 2.0^(-j) * dot(∇f(x), z_k - x) # Armijo linesearch 
-       #println(j, " ", stptest)
+    while γ > min_step 
+     stptest = f(x + 2.0^(-j) * (z_k - x)) - f(x) - σ * 2.0^(-j) * dot(∇f(x), z_k - x)  
  
-       if stptest > 0.0 # Armijo test   
+       if stptest > 0.0   
         j += 1 
         else
         γ = 2.0^(-j)
@@ -19,55 +18,61 @@ function GPA1(x, f, ∇f, projection, σ, min_step, γ_inicial, β_inicial)
        end       
     end
     
-    β = (- dot(z_k - x, ∇f(x)) * β^2) / (2.0 * (f(x + β * (z_k - x)) - f(x) - β * dot(z_k - x, ∇f(x))))
+    β = (- dot(z_k - x, ∇f(x)) * β^2) / (2.0 * (f(x + β * (z_k - x)) - f(x) - β * dot(z_k - x, ∇f(x)))) # Interpolation
     
     if β < β1 || β > β2
        β = β / 2.0
     end
     return (γ, β)
  
-    println("Step length too small!.")
- end
+    println("Step length too small!")
+end
 
- function method1(f, ∇f, x, ε, max_iter, GPA1)
+function method1(f, ∇f, ε, max_iter, GPA1)
 
     fvals = Float64[]
     gradnorms = Float64[]
     stepsizes_β = Float64[]
     stepsizes_γ = Float64[]
     iteration_time = Float64[]
+    seqx = Vector{Float64}[]
     
     # Initialization
     iter = 0
     x = x0
     t0 = time()
+
+    if norm(x - projection(x - ∇f(x))) < ε
+        println("x0 is a stationary point!")
+        return (x, f(x))
+    end
     
     while true
+        x_k = similar(x)
+        #push!(seqx, x_k)
         it0 = time()
         ∇fx = ∇f(x)
         fx = f(x) 
         gradnorm = norm(∇fx)
+        newsteps = GPA1(x, f, ∇f, projection, σ, min_step, γ_inicial, β_inicial)
+        z = projection(x - newsteps[2]  * ∇f(x)) 
+        x = x + newsteps[1] * (z - x)
+        it = time() - it0 
+        push!(iteration_time, it)
+        push!(stepsizes_β, newsteps[2])
+        push!(stepsizes_γ, newsteps[1])
+        push!(fvals, fx) 
+        push!(gradnorms, gradnorm)
         
         # First stopping condition
-        proj_stop = x - projection(x - ∇fx) 
-        if norm(proj_stop) < ε
+        if norm(x - x_k) < ε
            println("The solution has found!")
             break
         end
 
-        push!(fvals, fx) 
-        push!(gradnorms, gradnorm)
-        
         # Update iteration
         iter += 1
-        it = time() - it0
-        newsteps = GPA1(x, f, ∇f, projection, σ, imax_iter, γ_inicial, β_inicial) # GPA1
-        z = projection(x - newsteps[2]  * ∇f(x)) # GPA1
-        x = x + newsteps[1] * (z - x) # GPA1
-        push!(iteration_time, it)
-        push!(stepsizes_β, newsteps[2])
-        push!(stepsizes_γ, newsteps[1])
-        
+
         # Second stopping condition
         if iter > max_iter
             println("Maximum of iterations was achieved! Stopping...")
