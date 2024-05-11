2fd0b733cf6e797a0004f8a0c74557faf744a66f
EmanuelMQueiroz <106535637+EmanuelMQueiroz@users.noreply.github.com>
2024-05-11 10:39:41 -0300

    Update method2.jl

--- method2.jl
+++ method2.jl
@@ -5,7 +5,7 @@ function GPA2(x, f, ∇f, projection, σ, min_step, β_inicial)
     β = β_inicial
     j = 0
  
-    while β < min_step 
+    while β > min_step 
      z_kj = projection(x - β * 2.0^(-j) * ∇f(x)) # Calcula o ponto z_kj
      stptest = f(z_kj) - f(x) + σ * dot(∇f(x), x - z_kj) # Armijo
  
@@ -15,15 +15,16 @@ function GPA2(x, f, ∇f, projection, σ, min_step, β_inicial)
         j += 1 
         else
         β = β_inicial * 2.0^(-j)
-        return β
+        break
        end
-    
     end
+    
+    return β
  
     println("Step length too small!")
  end
 
- function method2(f, ∇f, x, ε, max_iter, GPA2)
+ function method2(f, ∇f, ε, max_iter, GPA2)
 
     fvals = Float64[]
     gradnorms = Float64[]
@@ -35,37 +36,41 @@ function GPA2(x, f, ∇f, projection, σ, min_step, β_inicial)
     iter = 0
     x = x0
     t0 = time()
+
+    if norm(x - projection(x - ∇f(x))) < ε
+        println("x0 is a stationary point!")
+        return (x, f(x))
+    end
     
     while true
+        x_k = similar(x)
         it0 = time()
         ∇fx = ∇f(x) 
         fx = f(x)
         gradnorm = norm(∇fx)
-        
-        # First stopping condition
-        proj_stop = x - projection(x - ∇fx) 
-        if norm(proj_stop) < ε
-            println("The solution has found!")
-            break
-        end
-
+        β = GPA2(x, f, ∇f, projection, σ, min_step, β_inicial)  
+        x = projection(x - β * ∇fx)
+        it = time() - it0
         push!(fvals, fx)
         push!(gradnorms, gradnorm)
-        
-        # Update iteration
-        iter += 1
-        it = time() - it0   
-        β = GPA2(x, f, ∇f, projection, σ, imax_iter, β_inicial)  
-        x = projection(x - β * ∇fx) 
         push!(iteration_time, it)
         push!(stepsizes_γ, 1.0)
         push!(stepsizes_β, β) 
         
+        # First stopping condition
+        if norm(x - x_k) < ε
+            println("The solution has found!")
+            break
+        end
+        
+        # Update iteration
+        iter += 1
+
         # Second stopping condition
         if iter > max_iter
             println("Maximum of iterations was achieved! Stopping...")
             break
-        end
+        end   
     end
 
     info = DataFrame(fvals = fvals, gradnorms = gradnorms, stepsizes_β = stepsizes_β, stepsizes_γ = stepsizes_γ, iteration_time = iteration_time)
