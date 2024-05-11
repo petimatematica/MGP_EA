7371548346afff733e018a51cb27bf6ec476e696
EmanuelMQueiroz <106535637+EmanuelMQueiroz@users.noreply.github.com>
2024-05-11 10:39:45 -0300

    Update method3.jl

--- method3.jl
+++ method3.jl
@@ -1,6 +1,6 @@
 ## Projected Gradient Method with GPA3 ##
 
- function method3(f, ∇f, ε, max_iter)
+function method3(f, ∇f, ε, max_iter)
 
     fvals = Float64[]
     gradnorms = Float64[]
@@ -9,36 +9,42 @@
     iteration_time = Float64[]
     
     # Initialization
-    iter = 0
+    iter = 1
     x = x0
+    β = β_inicial
     t0 = time()
+
+    if norm(x - projection(x - ∇f(x))) < ε
+        println("x0 is a stationary point!")
+        return (x, f(x))
+    end
     
     while true
+        x_k = similar(x)
         it0 = time()
         ∇fx = ∇f(x)
         fx = f(x)
-        gradnorm = norm(∇fx) 
+        gradnorm = norm(∇fx)
+        k = iter
+        α = 1/k   
+        β = α / gradnorm
+        x = projection(x - β * ∇fx)
+        it = time() - it0
+
+        push!(fvals, fx)
+        push!(gradnorms, gradnorm)
+        push!(stepsizes_β, β)
+        push!(iteration_time, it)
+        push!(stepsizes_γ, 1.0)
         
-        # First stopping condition
-        proj_stop = x - projection(x - ∇fx) 
-        if norm(proj_stop) < ε
+        # First stopping condition 
+        if norm(x - x_k) < ε
             println("The solution has found!")
             break
         end
-
-        push!(fvals, fx) 
-        push!(gradnorms, gradnorm) 
         
         # Update iteration
         iter += 1
-        it = time() - it0
-        k = iter
-        α = 1/k   
-        β = α / norm(∇f(x))  
-        x = projection(x - β * ∇fx)
-        push!(iteration_time, it)
-        push!(stepsizes_γ, 1.0)
-        push!(stepsizes_β, β)
 
         # Second stopping condition
         if iter > max_iter
