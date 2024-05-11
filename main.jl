d7323c09a42085cb3ae387ea24be73dd2a098e26
EmanuelMQueiroz <106535637+EmanuelMQueiroz@users.noreply.github.com>
2024-05-11 10:39:35 -0300

    Update main.jl

--- main.jl
+++ main.jl
@@ -1,4 +1,4 @@
-# Chamar todos os arquivos
+## MAIN ##
 
 include("dixonprice.jl")
 include("projections.jl")
@@ -8,35 +8,59 @@ include("method3.jl")
 
 using LinearAlgebra, DataFrames
 
-# Definição dos Parâmetros
+## Parameters choose ##
 
-x0 = [1, 1.9, 2] # Ponto inicial
+x0 = [1, 1.9]
 n = length(x0) # Dimensão da Função Dixon-Price
-y = [1, 1,0] # Centro do conjunto viável (Bola)
-δ = 1.0 # Raio da Bola
 σ = 1.e-4 # Parâmetro da Busca de Armijo
-ε = 1.e-4 # Critério de parada do Método
+ε = 1.e-5 # Critério de parada do Método
 β_inicial = 1.0 # Comprimento de passo inicial
 β1 = 1.e-6
 β2 = 1.0
 γ_inicial = 1.0
-min_step = 1.e-6
-max_iter = 100000 # Máximo de iteradas do Método
+min_step = 1.e-5
+max_iter = 10000
 strategy = "GPA3"
+feasible_set = "1"
+
+## Estrategy choose ##
 
 if strategy == "GPA1"
-   resultado = method1(f, ∇f, x0, ε, max_iter, GPA1) 
-elseif strategy == "GPA2"
-   resultado = method2(f, ∇f, x0, ε, max_iter, GPA2)
-elseif strategy == "GPA3"
-   resultado = method3(f, ∇f, x0, ε, max_iter) 
+   resultado = method1(f, ∇f, ε, max_iter, GPA1)
+   elseif strategy == "GPA2"
+   resultado = method2(f, ∇f, ε, max_iter, GPA2)
+   elseif strategy == "GPA3"
+   resultado = method3(f, ∇f, ε, max_iter) 
 end
 
-# Exibir o resultado
+## Feasible set choose ##
+
+if feasible_set == "1"
+   projection(x) = projection1(x)
+   elseif projection == "2"
+   projection(x) = projection2(x)
+   elseif projection == "3"
+   projection(x) = projection3(x)
+   elseif projection == "4"
+   projection(x) = projection4(x)
+   elseif projection == "5"
+   projection(x) = projection5(x)
+   elseif projection == "6"
+   projection(x) = projection6(x)
+end
+
+## Show the result ##
+
 ENV["LINES"] = 1000
 println(resultado[3])
 println("Real global minimum: ", global_minimum)
 println("Global minimum found: ", resultado[1])
 println("Minimum value of f: ", resultado[2])
-println("Time spent: ", resultado[4])
+println("Total time spent: ", resultado[4])
 
+## ORGANIZANDO OS TESTES ##
+
+projection_functions = [projection1, projection2, projection3, projection4, projection5, projection6]
+strategies = ["GPA1", "GPA2", "GPA3"]
+ndims = [5, 10, 50, 100, 500]
+initial_points = 
