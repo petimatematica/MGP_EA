d92ac15562a876b7724a2cba240250cd0c3322e7
EmanuelMQueiroz <106535637+EmanuelMQueiroz@users.noreply.github.com>
2024-05-24 14:33:48 -0300

    Update method1.jl

--- method1.jl
+++ method1.jl
@@ -1,14 +1,14 @@
 # ## Projected Gradient Method with GPA1 ##
 
-function GPA1(x, f, ∇f, projection, σ, min_step, γ_inicial, β_inicial)
+function GPA1(x, f, ∇f, projection, σ, min_step, γ_start, β_start)
     ierror = 0
-    β = β_inicial
-    γ = γ_inicial
+    β = β_start
+    γ = γ_start
     j = 0
-    z_k = projection(x - β * ∇f(x))
+    zk = projection(x - β * ∇f(x))
 
     while true 
-        stptest = f(x + 2.0^(-j) * (z_k - x)) - f(x) - σ * 2.0^(-j) * dot(∇f(x), z_k - x)  
+        stptest = f(x + 2.0^(-j) * (zk - x)) - f(x) - σ * 2.0^(-j) * dot(∇f(x), zk - x)  
         if stptest > 0.0   
            j += 1 
         else
@@ -16,12 +16,12 @@ function GPA1(x, f, ∇f, projection, σ, min_step, γ_inicial, β_inicial)
             if γ < min_step
                ierror = 1
                println("Step length too small!")
-            end
-            break 
+               break
+            end  
         end       
     end
     
-    β = (- dot(z_k - x, ∇f(x)) * β^2) / (2.0 * (f(x + β * (z_k - x)) - f(x) - β * dot(z_k - x, ∇f(x)))) # Interpolation
+    β = (-dot(zk - x, ∇f(x)) * β^2) / (2.0 * (f(x + β * (zk - x)) - f(x) - β * dot(zk - x, ∇f(x))))
     
     if β < β1 || β > β2
        β = β / 2.0
@@ -46,22 +46,22 @@ function method1(x0, f, ∇f, ε, max_iter, GPA1)
     t0 = time()
 
     if norm(x - projection(x - ∇f(x))) < ε
-        ierror = 3
         println("x0 is a stationary point!")
         return (x, f(x))
     end
     
     while true
-        x_k = copy(x)
+        xk = copy(x)
         it0 = time()
         ∇fx = ∇f(x)
         fx = f(x) 
         gradnorm = norm(∇fx)
-        newsteps = GPA1(x, f, ∇f, projection, σ, min_step, γ_inicial, β_inicial)
-        z = projection(x - newsteps[2]  * ∇f(x)) 
+        newsteps = GPA1(x, f, ∇f, projection, σ, min_step, γ_start, β_start)
+        z = projection(x - newsteps[2] * ∇f(x)) 
         x = x + newsteps[1] * (z - x)
         seqx = [seqx x] 
         it = time() - it0
+
         push!(iteration_time, it)
         push!(stepsizes_β, newsteps[2])
         push!(stepsizes_γ, newsteps[1])
@@ -70,7 +70,7 @@ function method1(x0, f, ∇f, ε, max_iter, GPA1)
         push!(gradnorms, gradnorm)
         
         # First stopping condition
-        if norm(x - x_k) < ε
+        if norm(x - xk) < ε
            println("The solution has found!")
             break
         end
